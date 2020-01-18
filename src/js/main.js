60a4e40e42d4d1fba64cf01f610322a1470edb1f
Charline <charline@iMac-de-moi.local>
2020-01-18 19:57:07 +0100

    modif couleurs logo

--- src/js/main.js
+++ src/js/main.js
@@ -105,4 +105,24 @@ $(document).ready(function () {
     } 
   });
 
+
+  //Smooth scroll footer nav
+  function scrollNavFooter() {
+    $('.footer_nav a').click(function(){  
+      //Toggle Class
+      $(".active").removeClass("active");      
+      $(this).closest('li').addClass("active");
+      var theClass = $(this).attr("class");
+      $('.'+theClass).parent('li').addClass('active');
+      //Animate
+      $('html, body').stop().animate({
+          scrollTop: $( $(this).attr('href') ).offset().top - 60
+      }, 400);
+      return false;
+    });
+    $('.scrollTop a').scrollTop();
+  }
+  scrollNavFooter();
+
+
 });
